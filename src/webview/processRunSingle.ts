1f55786cc00e680e6a1a5a833b47fa7792055337
cyrxdzj <2824671438@qq.com>
2024-10-11 15:53:07 +0800

    支持从文件读取答案。

--- src/webview/processRunSingle.ts
+++ src/webview/processRunSingle.ts
@@ -1,5 +1,5 @@
 import { Problem, RunResult } from '../types';
-import { getLanguage } from '../utils';
+import { getInputOriginFilenameFromInput, getLanguage } from '../utils';
 import { getBinSaveLocation, compileFile } from '../compiler';
 import { saveProblem } from '../parser';
 import { runTestCase, deleteBinary } from '../executions';
@@ -8,6 +8,8 @@ import * as vscode from 'vscode';
 import { getJudgeViewProvider } from '../extension';
 import { getIgnoreSTDERRORPref } from '../preferences';
 import telmetry from '../telmetry';
+import { readFile } from 'fs';
+import path from 'path';
 
 export const runSingleAndSave = async (
     problem: Problem,
@@ -56,6 +58,52 @@ export const runSingleAndSave = async (
             console.error('Failed to compile', problem, id);
             return;
         }
+    }
+
+    const input_origin_file_name=getInputOriginFilenameFromInput(testCase.input);
+    const answer_origin_file_name=getInputOriginFilenameFromInput(testCase.output);
+    if(input_origin_file_name.indexOf("/")!=-1)
+    {
+        vscode.window.showErrorMessage(
+            `For security reason, input_origin_file_name shouldn't contain '/'.`,
+        );
+        return;
+    }
+    if(answer_origin_file_name.indexOf("/")!=-1)
+    {
+        vscode.window.showErrorMessage(
+            `For security reason, answer_origin_file_name shouldn't contain '/'.`,
+        );
+        return;
+    }
+    if(input_origin_file_name!="")
+    {
+        const input_origin_file_path=path.join(path.parse(binPath).dir,input_origin_file_name);
+        await readFile(input_origin_file_path,(err,data)=>{
+            if(!err)
+            {
+                testCase.input=data.toString();
+            }
+            else
+            {
+                vscode.window.showErrorMessage("An error occurred when read input from "+input_origin_file_name+"\n"+err.stack);
+                console.error('ERR', err);
+            }
+        });
+    }
+    if(answer_origin_file_name!="")
+    {
+        const answer_origin_file_path=path.join(path.parse(binPath).dir,answer_origin_file_name);
+        await readFile(answer_origin_file_path,(err,data)=>{
+            if(!err)
+            {
+                testCase.output=data.toString();
+            }
+            else
+            {
+                console.log("An error occurred when read answer content from "+answer_origin_file_path+"\n"+err.stack);
+            }
+        });
     }
 
     const run = await runTestCase(language, binPath, testCase.input,input_file_name,output_file_name);
